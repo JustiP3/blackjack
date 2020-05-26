f67185d60e2ce7e8e83ca0280dd20e601eead5a0
justin <justinphil02@yahoo.com>
2020-05-26 12:16:17 -0700

    add div wrapper and basic css style

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(){
+    AppController.displayWelcome()
     let h1 = document.getElementsByTagName('h1')[0]
 
     h1.addEventListener('click', function(){
         h1.remove()
     })
 })
+
+class AppController {
+    static displayWelcome() {
+        const wrapper = document.getElementsByClassName('wrapper')[0]
+        const newGameDiv = document.createElement('div')
+        newGameDiv.setAttribute('class', 'button')
+        newGameDiv.innerText = "New Game"
+        wrapper.appendChild(newGameDiv)
+    }
+}
+
+class Game {
+
+}
+
+class Player {
+
+}
+
+class Statistics {
+
+}
