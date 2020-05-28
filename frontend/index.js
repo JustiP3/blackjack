d11aae525a98c79b1319e536c38a49a4b8e0892e
justin <justinphil02@yahoo.com>
2020-05-28 11:45:35 -0700

    add frontend Deck class and allCards variable

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     AppController.displayWelcome()
+    const wrapper = document.getElementsByClassName('wrapper')[0]
+    const newGameDiv = document.createElement('div')
+    newGameDiv.innerText = "New Game"
+    // wrapper.appendChild(newGameDiv)
+
+    debugger 
+
     let h1 = document.getElementsByTagName('h1')[0]
 
     h1.addEventListener('click', function(){
@@ -26,5 +33,274 @@ class Player {
 }
 
 class Statistics {
+
+}
+
+class Deck {
+    allCards = [
+        {
+            value: "Ace",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Two",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Three",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Four",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Five",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Six",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Seven",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Eight",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Nine",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Ten",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Jack",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "Queen",
+            suit: "Spades",
+            available: true 
+        },
+        {
+            value: "King",
+            suit: "Spades",
+            available: true 
+        },
+
+        {
+            value: "Ace",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Two",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Three",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Four",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Five",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Six",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Seven",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Eight",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Nine",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Ten",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Jack",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "Queen",
+            suit: "Clubs",
+            available: true 
+        },
+        {
+            value: "King",
+            suit: "Clubs",
+            available: true 
+        },
+
+        {
+            value: "Ace",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Two",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Three",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Four",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Five",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Six",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Seven",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Eight",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Nine",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Ten",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Jack",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "Queen",
+            suit: "Hearts",
+            available: true 
+        },
+        {
+            value: "King",
+            suit: "Hearts",
+            available: true 
+        },
+
+        {
+            value: "Ace",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Two",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Three",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Four",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Five",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Six",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Seven",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Eight",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Nine",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Ten",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Jack",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "Queen",
+            suit: "Diamonds",
+            available: true 
+        },
+        {
+            value: "King",
+            suit: "Diamonds",
+            available: true 
+        }       
+    ]
 
 }
