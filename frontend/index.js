14695f71ca54a6673cc2b6b60d4b7bacbd69929d
justin <justinphil02@yahoo.com>
2020-05-28 12:46:28 -0700

    remove placeholder content. add new game button

--- frontend/index.js
+++ frontend/index.js
@@ -1,26 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
     AppController.displayWelcome()
-    const wrapper = document.getElementsByClassName('wrapper')[0]
-    const newGameDiv = document.createElement('div')
-    newGameDiv.innerText = "New Game"
-    // wrapper.appendChild(newGameDiv)
+    
 
     debugger 
 
-    let h1 = document.getElementsByTagName('h1')[0]
 
-    h1.addEventListener('click', function(){
-        h1.remove()
-    })
 })
 
 class AppController {
     static displayWelcome() {
         const wrapper = document.getElementsByClassName('wrapper')[0]
+        
         const newGameDiv = document.createElement('div')
         newGameDiv.setAttribute('class', 'button')
         newGameDiv.innerText = "New Game"
         wrapper.appendChild(newGameDiv)
+
+        const viewStatsDiv = document.createElement('div')
+        viewStatsDiv.setAttribute('class', 'button')
+        viewStatsDiv.innerText = "View Statistics"
+        wrapper.appendChild(viewStatsDiv)
     }
 }
 
@@ -57,7 +56,7 @@ class Statistics {
     }
 
     displayStatsPage() {
-        
+
     }
 }
 
